92f7fc70382e9a33b05af03363a8ab0fee72c9f7
Alberto <ramirez-alberto@zoho.com>
2022-09-26 23:57:02 -0700

    Create a component for owner creation

--- AccountOwnerClient/src/app/owner/owner.module.ts
+++ AccountOwnerClient/src/app/owner/owner.module.ts
@@ -5,19 +5,26 @@ import { OwnerRoutingModule } from './owner-routing.module';
 import { OwnerListComponent } from './owner-list/owner-list.component';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
 import { OwnerAccountsComponent } from './owner-details/owner-accounts/owner-accounts.component';
+import { OwnerCreateComponent } from './owner-create/owner-create.component';
+
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 
 @NgModule({
   declarations: [
     OwnerListComponent,
     OwnerDetailsComponent,
-    OwnerAccountsComponent
+    OwnerAccountsComponent,
+    OwnerCreateComponent
   ],
   imports: [
     CommonModule,
     OwnerRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
+    BsDatepickerModule
   ]
 })
 export class OwnerModule { }
