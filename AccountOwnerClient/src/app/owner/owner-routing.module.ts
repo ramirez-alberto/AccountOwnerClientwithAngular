17cf1ca6f7b92752f1d0b7a250adb8561b629865
Ramirez <ramirez.alberto@zoho.com>
2022-09-27 17:13:50 -0700

    Fix routes

--- AccountOwnerClient/src/app/owner/owner-routing.module.ts
+++ AccountOwnerClient/src/app/owner/owner-routing.module.ts
@@ -5,12 +5,13 @@ import { OwnerListComponent } from './owner-list/owner-list.component';
 import { OwnerDetailsComponent } from './owner-details/owner-details.component';
 import { OwnerCreateComponent } from './owner-create/owner-create.component';
 import { OwnerDeleteComponent } from './owner-delete/owner-delete.component';
+import { OwnerUpdateComponent } from './owner-update/owner-update.component';
 
 const routes: Routes = [
   {path: "list",component:OwnerListComponent},
   {path: "details/:id", component:OwnerDetailsComponent},
   {path: "create", component:OwnerCreateComponent},
-  {path: "update/:id", component:OwnerCreateComponent},
+  {path: "update/:id", component:OwnerUpdateComponent},
   {path: "delete/:id", component:OwnerDeleteComponent}
 ];
 
